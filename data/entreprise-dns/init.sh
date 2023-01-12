07d531cc613dbebeb5198bc6dc40d11589f9ce42
wchrist94 <wuchristophe94@gmail.com>
2023-01-12 11:49:42 +0100

    adresse

---
+++ data/entreprise-dns/init.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+# Debug : affiche commandes dans le terminal
+set -x
+
+# On lui donne une adresse IP dans la DMZ
+  # ip flush dev eth0
+  # ip link set eth0 down
+  # ip addr add 120.0.16.21 dev eth0
+  # ip link set eth0 up
+
+# On lance le serveur
+  # rc-update add unbound
+  # rc-service unbound start
+
+ip r r default via 10.0.0.1
